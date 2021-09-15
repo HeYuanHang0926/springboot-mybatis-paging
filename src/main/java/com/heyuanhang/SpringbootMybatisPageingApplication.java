3be34a555bc3e60e8af777b80ccdb115b52eb342
HeYuanHang <3145083560@qq.com>
2021-09-15 10:00:40 +0800

    构建了一个初始化的springBoot整合mybatis以及分页的空项目

---
+++ src/main/java/com/heyuanhang/SpringbootMybatisPageingApplication.java
@@ -0,0 +1,13 @@
+package com.heyuanhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisPageingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisPageingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heyuanhang/SpringbootMybatisPageingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heyuanhang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisPageingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
